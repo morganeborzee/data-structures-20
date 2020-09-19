36ec23223f37c38a541aace8c6aff44fc8c24410
mborzee <53446985+morganeborzee@users.noreply.github.com>
2020-09-19 11:41:53 -0500

    Create wa03.js

---
+++ wa03/wa03.js
@@ -0,0 +1,62 @@
+"use strict"
+
+// dependencies
+const fs = require('fs'),
+      querystring = require('querystring'),
+      request = require('request'),
+      async = require('async'),
+      dotenv = require('dotenv');
+
+// TAMU api key
+dotenv.config();
+const API_KEY = process.env.MY_API;
+const API_URL = 'https://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx'
+
+// geocode addresses
+let meetingsData = [];
+let addresses = JSON.parse(fs.readFileSync('data/addresses.json'));
+
+
+// eachSeries in the async module iterates over an array and operates on each item in the array in series
+async.eachSeries(addresses, function(value, callback) {
+    let query = {
+        streetAddress: value,
+        city: "New York",
+        state: "NY",
+        apikey: API_KEY,
+        format: "json",
+        version: "4.01"
+    };
+
+    // construct a querystring from the `query` object's values and append it to the api URL
+    let apiRequest = API_URL + '?' + querystring.stringify(query);
+
+    request(apiRequest, function(err, resp, body) {
+        if (err){ throw err; }
+
+        let tamuGeo = JSON.parse(body);
+        console.log(tamuGeo['FeatureMatchingResultType'], apiRequest);
+        console.log(body);
+        
+        const latLong = []
+        
+        latLong.push(tamuGeo['OutputGeocodes'][0]['OutputGeocode']['Latitude']);
+        latLong.push(tamuGeo['OutputGeocodes'][0]['OutputGeocode']['Longitude']);
+        latLong.push(tamuGeo['InputAddress']);
+        
+        console.log(latLong);
+        meetingsData.push(latLong);
+        
+    });
+
+    // sleep for a couple seconds before making the next request
+    setTimeout(callback, 2000);
+    
+
+}, function() {
+    
+    
+    fs.writeFileSync('data/first.json', JSON.stringify(meetingsData));
+    console.log('*** *** *** *** ***');
+    console.log(`Number of meetings in this zone: ${meetingsData.length}`);
+});
